1e810c0bbfbe04e3360eb2af3e71e61753499b1e
Sophia Khan <sophiaxkhan@gmail.com>
2017-02-13 13:25:05 -0500

    Added about me file

---
+++ js/about-me.js
@@ -0,0 +1,14 @@
+var firstName = 'Sophie';
+var lastName = 'Khan';
+var fullName = firstName + ' ' + lastName;
+var email = 'sophiaxkhan@gmail.com';
+var indianapolis = false;
+var collegeYears = 4;
+var petsAmount = 1;
+var hoursSleep = parseFloat('7');
+var favGenre = 'Changes every day!'
+var favShowsMovies = ['30Rock', 'The Addams Family']
+var jobTitle 
+var withoutJS = null;
+
+console.log(fullName);
---
+++ js/global.js
@@ -0,0 +1,10 @@
+// var fullName = 'Sophie Khan';
+// var action1 = 'ran';
+// var noun1 = 'toothbrush';
+
+// var story = 'One day, ' + fullName + ' ' + action1 + ' over the ' + noun1;
+
+// var story = `One day, ${fullName} ${action1} over the ${noun1}.`;
+
+// console.log(story); 
+
