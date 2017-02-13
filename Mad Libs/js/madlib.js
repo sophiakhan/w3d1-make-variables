82ef8debe6cac400bcf22586e3c449d0780cb2b2
Sophia Khan <sophiaxkhan@gmail.com>
2017-02-13 15:21:36 -0500

    Commiting final lib

--- Mad Libs/js/madlib.js
+++ Mad Libs/js/madlib.js
@@ -1,25 +1,27 @@
-var noun1 = '';
-var place1 = '';
-var sillyWord = '';
-var verb1 = '';
-var noun2 = '';
-var verb2 = '';
-var adj1 = '';
-var adj2 = '';
-var number = '';
-var adj3 = '';
-var adverb1 = '';
-var adj4 = '';
-var adj5 = '';
-var number2 = '';
-var measureTime = '';
-var adj6 = '';
-var adj7 = '';
-var adj8 = '';
-var noun3 = '';
-var number3 = '';
-var number4 = '';
+var noun1 = 'Stapler';
+var place1 = 'Venice Beach';
+var sillyWord = 'underpants';
+var verb1 = 'squirt';
+var noun2 = 'Beyonce';
+var verb2 = 'jump';
+var adj1 = 'furry';
+var adj2 = 'stinky';
+var number = '247';
+var adj3 = 'delectable';
+var adverb1 = 'rapidly';
+var adj4 = 'green';
+var adj5 = 'spikey';
+var number2 = '2';
+var measureTime = 'weeks';
+var adj6 = 'creamy';
+var adj7 = 'moist';
+var adj8 = 'radical';
+var noun3 = 'Portland';
+var number3 = '40';
+var number4 = '10,000';
 
-var story = `Is your $[noun1] making a mess of your ${place1}? Try ${sillyWord}! It will ${verb1} your whole ${noun2} and ${verb2} to unbelievable levels! Watch, as we demonstrate, the ${adj1} capabilities of this ${adj2} product! Everyone should have at least ${number} of these.
+var story = `Is your ${noun1} making a mess of your ${place1}? Try ${sillyWord}! It will ${verb1} your whole ${noun2} and ${verb2} to unbelievable levels! Watch, as we demonstrate, the ${adj1} capabilities of this ${adj2} product! Everyone should have at least ${number} of these.
 
-To order, call the number on your screen. ${adj3} phone attendants will ${adverb1} take your request. ${adj4} and ${adj5} service will be yours! Call within the next ${number2} ${measureTime}, and you will receive a ${adj6}, ${adj7}, ${adj8} ${noun3} as a bonus, absolutely free for an additional value of ${number3} dollars, plus shipping and handling cost of ${number4} dollars. Act now!`
+To order, call the number on your screen. ${adj3} phone attendants will ${adverb1} take your request. ${adj4} and ${adj5} service will be yours! Call within the next ${number2} ${measureTime}, and you will receive a ${adj6}, ${adj7}, ${adj8} ${noun3} as a bonus, absolutely free for an additional value of ${number3} dollars, plus shipping and handling cost of ${number4} dollars. Act now!`;
+
+console.log(story);
