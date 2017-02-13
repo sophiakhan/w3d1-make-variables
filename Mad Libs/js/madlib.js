49ac94de1e7058cfbdceec154b41ec524cca298b
Sophia Khan <sophiaxkhan@gmail.com>
2017-02-13 14:56:09 -0500

    Added story variable

--- Mad Libs/js/madlib.js
+++ Mad Libs/js/madlib.js
@@ -20,3 +20,6 @@ var noun3 = '';
 var number3 = '';
 var number4 = '';
 
+var story = `Is your $[noun1] making a mess of your ${place1}? Try ${sillyWord}! It will ${verb1} your whole ${noun2} and ${verb2} to unbelievable levels! Watch, as we demonstrate, the ${adj1} capabilities of this ${adj2} product! Everyone should have at least ${number} of these.
+
+To order, call the number on your screen. ${adj3} phone attendants will ${adverb1} take your request. ${adj4} and ${adj5} service will be yours! Call within the next ${number2} ${measureTime}, and you will receive a ${adj6}, ${adj7}, ${adj8} ${noun3} as a bonus, absolutely free for an additional value of ${number3} dollars, plus shipping and handling cost of ${number4} dollars. Act now!`
