b102674f69aa6ed8866c7f5aa6877f4c7ca4254c
Sophia Khan <sophiaxkhan@gmail.com>
2017-02-13 14:05:41 -0500

    Added variables

--- js/about-me.js
+++ js/about-me.js
@@ -4,6 +4,7 @@ var fullName = firstName + ' ' + lastName;
 var email = 'sophiaxkhan@gmail.com';
 var indianapolis = false;
 var collegeYears = 4;
+var gradDate = new Date (2010, 4, 15);
 var petsAmount = 1;
 var hoursSleep = parseFloat('7');
 var favGenre = 'Changes every day!'
@@ -11,4 +12,17 @@ var favShowsMovies = ['30Rock', 'The Addams Family']
 var jobTitle 
 var withoutJS = null;
 
+console.log(firstName);
+console.log(lastName);
 console.log(fullName);
+console.log(email);
+console.log(indianapolis);
+console.log(collegeYears);
+console.log(gradDate);
+console.log(petsAmount);
+console.log(hoursSleep);
+console.log(favGenre);
+console.log(favShowsMovies);
+console.log(jobTitle);
+console.log(withoutJS);
+
